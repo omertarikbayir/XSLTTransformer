2a8ab0a06cdf41feac62f3acef658b18f1e3363b
omertarikbayir <omrtrkbyr@gmail.com>
2026-05-19 00:40:38 +0300

    Add files via upload

---
+++ XSLTTransformer.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using XSLTTransformer.Core;
+
+namespace XSLTTransformer.Wpf;
+
+public partial class MainWindow : Window
+{
+    private readonly XsltEngine _engine = new();
+    private bool _isTr = true;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        SetTurkish(null, null);
+    }
+
+    private void SetEnglish(object s, RoutedEventArgs e)
+    {
+        _isTr = false;
+        XmlInput.Text = "Paste XML here";
+        XsltInput.Text = "Paste XSLT here";
+        StatusText.Text = "Ready";
+    }
+
+    private void SetTurkish(object s, RoutedEventArgs e)
+    {
+        _isTr = true;
+        XmlInput.Text = "XML buraya yapıştırın";
+        XsltInput.Text = "XSLT buraya yapıştırın";
+        StatusText.Text = "Hazır";
+    }
+
+    private void OnTransform(object s, RoutedEventArgs e)
+    {
+        try
+        {
+            var output = _engine.Transform(XmlInput.Text, XsltInput.Text);
+            ResultOutput.Text = output;
+            StatusText.Text = _isTr ? "Başarılı" : "Success";
+        }
+        catch (System.Exception ex)
+        {
+            ResultOutput.Text = ex.Message;
+            StatusText.Text = _isTr ? "Hata" : "Error";
+        }
+    }
+}
